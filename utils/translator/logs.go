6ded7ec1a900f3b7570ffed3fac4c2aa67c91f42
Avadhut Pisal <avadhutpisal47@gmail.com>
2023-02-21 17:36:01 +0530

    make log body handling generic

--- utils/translator/logs.go
+++ utils/translator/logs.go
@@ -4,10 +4,10 @@ import (
 	"github.com/logicmonitor/lm-data-sdk-go/model"
 )
 
-func ConvertToLMLogInput(logMessage interface{}, timestamp string, resourceidMap, metadata map[string]interface{}) model.LogInput {
+func ConvertToLMLogInput(logMessage interface{}, timestamp string, resourceIDMap, metadata map[string]interface{}) model.LogInput {
 	return model.LogInput{
 		Message:    logMessage,
-		ResourceID: resourceidMap,
+		ResourceID: resourceIDMap,
 		Metadata:   metadata,
 		Timestamp:  timestamp,
 	}
